a5f8d240a9f028ad14f82033c732cf3e4fe52f29
yong1301 <yong1301@ajou.ac.kr>
2017-10-06 18:02:58 +0900

    commands.c is implemented and assigment0 is completed

--- src/commands.c
+++ src/commands.c
@@ -1,21 +1,38 @@
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
+  int result;
+
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  result = chdir(argv[1]);
+  
+  if(result != 0)
+      return -1;
+
   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
+  char* cwd;
+
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  cwd = getcwd(NULL, 0);
 
+  if(cwd)
+    printf("%s\n", cwd);
+  else
+    return -1;
+
+  free(cwd);
   return 0;
 }
 
@@ -28,7 +45,6 @@ int validate_cd_argv(int argc, char** argv) {
   }
   else
     return 0;
-  return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
@@ -40,5 +56,4 @@ int validate_pwd_argv(int argc, char** argv) {
   }
   else
     return 0;
-  return 1;
 }
